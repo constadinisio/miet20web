bf330490a62404bfb5a745006c8db2f40e3fe13e
Constantino Di Nisio <165814786+constadinisio@users.noreply.github.com>
2025-06-20 01:02:06 -0300

    Versión 1.5.5 - Añadiendo credenciales a .env

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-// Define las credenciales de la base de datos a la que se conectará.
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "et20plataforma";
+require_once __DIR__ . './loadEnv.php';
+cargarEntorno(__DIR__ . '/../.env');
 
-// Crea una variable conexión, creando una conexión a las credenciales que definimos.
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$database = $_ENV['DB_NAME'];
+
 $conexion = new mysqli($host, $user, $password, $database);
 
-// Si la conexión marca error esta se detiene mostrando la razón por la cual se detuvo.
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
