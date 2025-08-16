3c9a6cdf14d64a74d1c8e96ee92f9281e946530e
Constantino Di Nisio <165814786+constadinisio@users.noreply.github.com>
2025-08-15 21:16:11 -0300

    Update login.php

--- public/login.php
+++ public/login.php
@@ -10,7 +10,7 @@ $csrf = $_SESSION['csrf'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi ET20 - Sistema Login</title>
+    <title>Mi ET20 - Sistema de Login</title>
     <link rel="icon" type="image/x-icon" href="./images/et20png.png">
     <link rel="stylesheet" href="./output.css" />
     <!-- Google Fonts -->
